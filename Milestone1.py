1cce1e4d135e5317df868b862da2be857c01845d
gbacon255 <116466493+gbacon255@users.noreply.github.com>
2022-10-23 18:05:45 +0000

    Update Milestone1.py

--- Milestone1.py
+++ Milestone1.py
@@ -1,28 +1,6 @@
-import getPass
+import getpass
+import sys
 
-
-# prompts the user to choose yes or no to creating new account
-# to handle user input in this program, the input function was used.
-userRegister = input('Do you want to register a new account: (y/n)')
-#if user chooses to generate a new account
-if(userRegister == 'y') or (userRegister == 'Y'):
-  # Grabs user full name
-  userFullName = input('Enter Full Name:\n')
-  # Grabs User email address
-  userEmail = input('Enter Email Address:\n')
-  # Grabs user password
-  password = getPassword()
-  print('Registration Successful\n')
-  # Opens file for user data storage, currently implemented with basic text file
-  file = open('UserInfo.txt', 'wb')
-  # prints plaintext user data to file format "Full Name Email Password")
-  file.print(userFullName, ' ', userEmail, ' ', password)
-else:
-   # currently implemented to exit program upon user choosing not to register
-   exit();
-    
-    
-# function to grab user password, returns password    
 def getPassword():
   # while loop set to run until same strings are entered
   while(1):
@@ -35,5 +13,29 @@ def getPassword():
     password2 = getpass.getpass()
     #compares the two passwords entered and if they are the same, while loop breaks and password is returned
     if userPassword == password2:
-      break
+        break
   return userPassword
+
+
+# prompts the user to choose yes or no to creating new account
+# to handle user input in this program, the input function was used.
+userRegister = input('Do you want to register a new account: (y/n)')
+#if user chooses to generate a new account
+if(userRegister == 'y') or (userRegister == 'Y'):
+    # Grabs user full name
+    userFullName = input('Enter Full Name:\n')
+    # Grabs User email address
+    userEmail = input('Enter Email Address:\n')
+    # Grabs user password
+    password = getPassword()
+    print('Registration Successful\n')
+    # Opens file for user data storage, currently implemented with basic text file
+    f = open('UserInfo.txt', 'a')
+    # prints plaintext user data to file format "Full Name Email Password")
+    print(userFullName, ' ', userEmail, ' ', password,'\n', file = f)
+    f.close()
+else:
+# currently implemented to exit program upon user choosing not to register
+   exit()
+                                        
+
